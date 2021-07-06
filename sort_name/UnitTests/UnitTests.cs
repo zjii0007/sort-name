1b8cf6ce9c413a317562ef538b52b016a2e36ef5
zjii0007 <daju12138@gmail.com>
2021-07-06 21:57:42 +1000

    first commit

---
+++ sort_name/UnitTests/UnitTests.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using sort_name;
+using System.Linq;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTests
+{
+    public class Tests
+    {
+		
+
+		/// The only test:
+		/// Read the given file and check that each name is lexically after the previous.
+		[Test]
+		public void TestSortName()
+		{
+			string[] names = { "Vane Bruce", "Emma Alice", "Kent Clark" };
+			var name = sortName.sortedNames(names);
+			Assert.AreEqual("Emma Alice", name[0]);
+			Assert.AreEqual("Vane Bruce", name[1]);
+			Assert.AreEqual("Clark Kent", name[2]);
+
+		}
+
+		[Test]
+		public void TesttheLastToFirst()
+		{
+			string example = "Kent Clark";
+			string newName = sortName.theLastToFirst(example);
+			Assert.IsTrue(newName.Equals("Clark Kent"));
+			
+		}
+
+	}
+}
---
+++ sort_name/sort_name/sortName.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+
+namespace sort_name
+{
+	public class sortName
+	{
+		/// Entry point
+		public static void Main()
+		{
+			Console.Write("Enter Integer: ");
+			string args = Console.ReadLine();
+			while (!args.Equals("name-sorter ./unsorted-names-list.txt"))
+			{
+				Console.WriteLine("Invalid command! Useage: name-sorter ./unsorted-names-list.txt");
+				Console.Write("Enter Integer: ");
+				args = Console.ReadLine();
+			}
+
+			// Take each argument as a filename:
+			// read the file
+			var readFile = System.IO.File.ReadAllLines(args);
+			var names = sortedNames(readFile);
+			foreach (string name in names)
+			{
+				System.Console.WriteLine(name);
+			}
+			System.IO.File.WriteAllLines(@"sorted-names-list.txt", names);
+
+		}
+
+		public static string[] sortedNames(string[] names)
+		{
+			
+			// get a parallel array for sorting
+			var sortKey = names.Select(theLastToFirst).ToArray();
+			System.Array.Sort(sortKey, names);
+			return names;
+		}
+
+		
+
+		// param: the name inside .txt 
+		// return the name with the surname placed at the front
+		public static string theLastToFirst(string name)
+		{
+			// split name to single word
+			var names = name.Split(' ', System.StringSplitOptions.None);
+			var length = names.Length;
+			// put the surname at the front
+			return names[length - 1] + ' ' + string.Join(' ', names, 0, length - 1);
+		}
+	}
+}
